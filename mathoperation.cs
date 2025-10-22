e2faaa1eadab88a938bf861801131a4a2b2c8658
csharplearner07 <csharplearner07@gmail.com>
2025-10-22 21:10:31 +0500

    Add mathoperation.cs for basic arithmetic operations

---
+++ mathoperation.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = a + 2;
+            int c = a * b;
+            Console.WriteLine(c);
+        }
+    }
+}
